f7b9de25711983f0ad77983ef4465d592c394bb3
Anthony Volk <anth.volk@gmail.com>
2022-12-15 19:30:32 +0800

    Built UnderConstruction

---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,10 @@
+import './UnderConstruction.css';
+import '../index.css';
+
+export default function UnderConstruction() {
+    return (
+        <div className="underConstWrapper">
+            <p className="underConstMsg">UNDER CONSTRUCTION</p>
+        </div>
+    );
+}
