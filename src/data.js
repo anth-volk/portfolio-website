12986f405e5ed2b7f5a3a8c7c22a55159b6db342
Anthony Volk <anth.volk@gmail.com>
2023-01-09 08:56:28 +0800

    Added project to database

--- src/data.js
+++ src/data.js
@@ -1,18 +1,18 @@
 export const projectData = [
     {
-        name: "Portfolio Website",
-        description: "Single-page personal portfolio website built as a series of React components within one React app",
+        name: "Drum Machine",
+        description: "Drum machine application built in React using styled-components to practice asynchronous state management",
         techTags: [
             "React",
-            "HTML",
-            "CSS"
+            "Styled-Components",
+            "HTML"
         ],
-        image: "../img/portfolio.png",
-        link: "http://www.anthonyvolk.com"
+        image: "../img/drum_machine.png",
+        link: "https://q4c0ni.csb.app/"
     },
     {
         name: "Random Quote Generator",
-        description: "React applet that displays quote fetched from GitHub and allows user to Tweet it (FreeCodeCamp project)",
+        description: "React applet that displays quote fetched from GitHub and allows user to Tweet it; created to learn async API fetching",
         techTags: [
             "React",
             "HTML",
@@ -23,7 +23,7 @@ export const projectData = [
     },
     {
         name: "Markdown Previewer",
-        description: "Application using React that parses markdown and displays rendered preview (FreeCodeCamp project)",
+        description: "Application using React that parses markdown and displays rendered preview; used to review basic state management",
         techTags: [
             "React",
             "HTML",
@@ -31,6 +31,17 @@ export const projectData = [
         ],
         image: "../img/markdown_previewer.png",
         link: "https://sr4um5.csb.app/"
-    }
+    },
+    {
+        name: "Portfolio Website",
+        description: "Single-page personal portfolio website built as a series of React components within one React app",
+        techTags: [
+            "React",
+            "HTML",
+            "CSS"
+        ],
+        image: "../img/portfolio.png",
+        link: "http://www.anthonyvolk.com"
+    },
 
 ];
