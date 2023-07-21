b7843577b1a8c082052074abb5e67ea67adced3e
Anthony Volk <anth.volk@gmail.com>
2023-07-20 23:38:27 -0600

    feat: Updated nav desktop links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,17 +44,14 @@ export default function Navbar(props) {
     else {
 
         return (
-            <nav className={props.visible ? "Navbar visible" : "Navbar hidden"} id="navbar">
+            <nav className="Navbar" id="navbar">
                 <a href="#home"><p className="Navbar_logo">ANTHONY VOLK</p></a>
                 <div className="Navbar_navList">
                     <ul>
                         <a href="#about"><li className="Navbar_link">about</li></a>
-                        <a href="#projects"><li className="Navbar_link">projects</li></a>
+                        <a href="#work"><li className="Navbar_link">work</li></a>
                         <a href="#contact"><li className="Navbar_link">contact</li></a>
                     </ul>
-
-
-
                 </div>
             </nav>
         );
