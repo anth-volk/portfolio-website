5217df77592251bf0659cae85e4859a813cf75d8
Anthony Volk <anth.volk@gmail.com>
2023-07-26 19:14:41 -0600

    feat: Text portion of headline component

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,5 +1,12 @@
+// Internal imports
+import '../styles_components/Headline.css';
+
 export default function Headline() {
+
 	return (
-		<></>
+		<div className='Headline'>
+			<h1 className='Headline_text'>Anthony Volk.</h1>
+			<h1 className='Headline_text'>Full-Stack Developer.</h1>
+		</div>
 	)
 }
