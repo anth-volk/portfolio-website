f5af6bc0f758551651f42cf821864bb73885c15f
Anthony Volk <anth.volk@gmail.com>
2023-08-04 18:12:36 -0600

    feat: Half-implementation of tag clouds for home page

--- src/components/TagCloud.js
+++ src/components/TagCloud.js
@@ -1,5 +1,45 @@
+
+
+// Local imports
+import { tagCloudData } from '../data/tagCloudData.js';
+
 export default function TagCloud() {
+
+	// Use the "headers" from the tag cloud data to create a row of three
+	// hoverable links; this will be placed above the tag cloud section
+	const tagCloudHeaders = tagCloudData.map( (obj) => {
+
+		return (
+			<p className='TagCloud_header'>{obj.header.toLowerCase()}</p>
+		);
+
+	});
+
+	// Create a wrapper for each tag cloud; these will be positioned
+	// absolutely and sit on top of one another
+	const tagCloudTagsWrapper = tagCloudData.map( (obj) => {
+
+		// First, place all tags into a p tag
+		const tagCloudTags = obj.tags.map( (tag) => {
+
+			return (
+				<p className='TagCloud_tag'>{tag}</p>
+			);
+		});
+
+		// Then integrate all tags into the wrapper
+		return (
+			<div className='TagCloud_cloud'>
+				{tagCloudTags}
+			</div>
+		);
+
+	});
+
 	return (
-		<></>
+		<>
+			{tagCloudHeaders}
+			{tagCloudTagsWrapper}
+		</>
 	)
 }
---
+++ src/data/tagCloudData.js
@@ -0,0 +1,8 @@
+export const tagCloudData = [
+	{
+		header: ,
+		tags: [
+
+		]
+	}
+];
