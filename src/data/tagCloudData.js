cdd3e1aec897f504675f2c77ae28f3b514e633ce
Anthony Volk <anth.volk@gmail.com>
2023-08-11 22:49:46 -0600

    feat: Tag cloud data added

--- src/data/tagCloudData.js
+++ src/data/tagCloudData.js
@@ -1,8 +1,37 @@
 export const tagCloudData = [
 	{
-		header: ,
+		header: 'mission',
+		content: `I'm a software engineer and international relations
+		professional with a passion for building digital tools that
+		empower people`
+	},
+	{
+		header: 'skills',
 		tags: [
-
+			'javascript',
+			'html',
+			'css',
+			'wireframing',
+			'figma',
+			'sql',
+			'node',
+			'express',
+			'react',
+			'strategy',
+			'python',
+			'flask',
+			'c'
+		]
+	},
+	{
+		header: 'interests',
+		tags: [
+			'travel',
+			'languages',
+			'foreign policy',
+			'politics',
+			'hiking',
+			'fermentation'
 		]
 	}
 ];
