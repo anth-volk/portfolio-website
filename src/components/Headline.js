b9f12d1585c163e1d1a8430a13387215ceece7f5
Anthony Volk <anth.volk@gmail.com>
2023-08-13 00:04:55 -0600

    feat: Colors added to scrolling headlines

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -18,29 +18,29 @@ export default function Headline() {
 
 	return (
 		<div className='Headline'>
-			<h1 className='Headline_text Headline_text--noAbs'>Anthony Volk.</h1>
+			<h1 className='Headline_text App_light'>Anthony Volk.</h1>
 			<div className='Headline_scrollContainer'>
 				<div 
 					className='Headline_scrollWrapper' 
 					onMouseOver={handleMouseOver} 
 					onMouseOut={handleMouseOut}
 				>
-					<h1 className='Headline_text'>Full-Stack Developer.</h1>
-					<h1 className={`Headline_text ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Ethical Technologist.</h1>
-					<h1 className={`Headline_text ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Culture Junkie.</h1>
-					<h1 className={`Headline_text ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Language Learner.</h1>
-					<h1 className={`Headline_text ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Cat Lover.</h1>
+					<h1 className='Headline_text App_light'>Full-Stack Developer.</h1>
+					<h1 className={`Headline_text App_red ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Ethical Technologist.</h1>
+					<h1 className={`Headline_text App_blue ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Culture Junkie.</h1>
+					<h1 className={`Headline_text App_yellow ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Language Learner.</h1>
+					<h1 className={`Headline_text App_red ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Cat Lover.</h1>
 				</div>
 				<div 
 					className='Headline_scrollWrapper' 
 					onMouseOver={handleMouseOver} 
 					onMouseOut={handleMouseOut}
 				>
-					<h1 className={`Headline_text ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Full-Stack Developer.</h1>
-					<h1 className={`Headline_text ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Ethical Technologist.</h1>
-					<h1 className={`Headline_text ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Culture Junkie.</h1>
-					<h1 className={`Headline_text ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Language Learner.</h1>
-					<h1 className={`Headline_text ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Cat Lover.</h1>
+					<h1 className={`Headline_text App_light ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Full-Stack Developer.</h1>
+					<h1 className={`Headline_text App_red ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Ethical Technologist.</h1>
+					<h1 className={`Headline_text App_blue ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Culture Junkie.</h1>
+					<h1 className={`Headline_text App_yellow ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Language Learner.</h1>
+					<h1 className={`Headline_text App_red ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Cat Lover.</h1>
 				</div>
 			</div>
 		</div>
