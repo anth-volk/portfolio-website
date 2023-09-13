3ec0f7f82ddda2767bcbab02451a0b9438d7964e
Anthony Volk <anth.volk@gmail.com>
2023-09-13 16:53:24 -0600

    feat: Beginning of bouncing ellipsis feature

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -25,22 +25,29 @@ export default function Headline() {
 					onMouseOver={handleMouseOver} 
 					onMouseOut={handleMouseOut}
 				>
-					<h1 className='Headline_text App_light Headline_small'>Full-Stack Developer.</h1>
-					<h1 className={`Headline_text Headline_small App_red ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Ethical Technologist.</h1>
-					<h1 className={`Headline_text Headline_small App_blue ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Culture Junkie.</h1>
-					<h1 className={`Headline_text Headline_small App_yellow ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Language Learner.</h1>
-					<h1 className={`Headline_text Headline_small App_red ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Cat Lover.</h1>
+					<h1 className='Headline_text App_light Headline_small'>Full-Stack Developer.
+            <span className={`Headline_text App_light Headline_small Headline_hint Headline_hint--1 ${isMouseOver && 'Headline_text--hidden'}`}>.
+            </span>
+            <span className={`Headline_text App_light Headline_small Headline_hint Headline_hint--2 ${isMouseOver && 'Headline_text--hidden'}`}>.
+            </span>
+            <span className={`Headline_text App_light Headline_small Headline_hint Headline_hint--3 ${isMouseOver && 'Headline_text--hidden'}`}>.
+            </span>
+          </h1>
+					<h1 className={`Headline_text Headline_small App_red ${!isMouseOver && `Headline_text--hidden`}`}>Ethical Technologist.</h1>
+					<h1 className={`Headline_text Headline_small App_blue ${!isMouseOver && `Headline_text--hidden`}`}>Culture Junkie.</h1>
+					<h1 className={`Headline_text Headline_small App_yellow ${!isMouseOver && `Headline_text--hidden`}`}>Language Learner.</h1>
+					<h1 className={`Headline_text Headline_small App_red ${!isMouseOver && `Headline_text--hidden`}`}>Cat Lover.</h1>
 				</div>
 				<div 
 					className='Headline_scrollWrapper' 
 					onMouseOver={handleMouseOver} 
 					onMouseOut={handleMouseOut}
 				>
-					<h1 className={`Headline_text Headline_small App_light ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Full-Stack Developer.</h1>
-					<h1 className={`Headline_text Headline_small App_red ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Ethical Technologist.</h1>
-					<h1 className={`Headline_text Headline_small App_blue ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Culture Junkie.</h1>
-					<h1 className={`Headline_text Headline_small App_yellow ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Language Learner.</h1>
-					<h1 className={`Headline_text Headline_small App_red ${!isMouseOver ? `Headline_text--hidden` : ``}`}>Cat Lover.</h1>
+					<h1 className={`Headline_text Headline_small App_light ${!isMouseOver && `Headline_text--hidden`}`}>Full-Stack Developer.</h1>
+					<h1 className={`Headline_text Headline_small App_red ${!isMouseOver && `Headline_text--hidden`}`}>Ethical Technologist.</h1>
+					<h1 className={`Headline_text Headline_small App_blue ${!isMouseOver && `Headline_text--hidden`}`}>Culture Junkie.</h1>
+					<h1 className={`Headline_text Headline_small App_yellow ${!isMouseOver && `Headline_text--hidden`}`}>Language Learner.</h1>
+					<h1 className={`Headline_text Headline_small App_red ${!isMouseOver && `Headline_text--hidden`}`}>Cat Lover.</h1>
 				</div>
 			</div>
 		</div>
