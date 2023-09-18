635eeba9a3e60e6f1dca5ae551c6c11afe2e649f
Anthony Volk <anth.volk@gmail.com>
2023-09-18 14:42:30 -0600

    fix: Headline scroller works properly for mobile breakpoints

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,5 +1,5 @@
 // External imports
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Internal imports
 import { useBreakpoint } from '../hooks/hooks.js';
@@ -22,6 +22,46 @@ export default function Headline() {
     setIsMouseOver(prev => !prev);
   }
 
+  useEffect(() => {
+    if (breakpoint === 'mobile') {
+
+      const scrollWrappers = document.querySelectorAll('.Headline_scrollWrapper');
+
+      if (isMouseOver) {
+
+        const textScroller = [
+          { transform: 'translateX(0%)' },
+          { transform: 'translateX(-100%)' }
+        ];
+
+        const textScrollerOptions = {
+          duration: 8000,
+          iterations: Infinity,
+          easing: 'linear'
+        };
+
+        scrollWrappers.forEach((wrapper) => {
+          wrapper.animate(
+            textScroller,
+            textScrollerOptions
+          );
+        });
+
+      }
+      else {
+        scrollWrappers.forEach((wrapper) => {
+          const animations = wrapper.getAnimations();
+
+          if (animations.length) {
+            animations.forEach((animation => animation.cancel()));
+          }
+        });
+      }
+
+
+    }
+  }, [isMouseOver, breakpoint])
+
 	return (
 		<div className='Headline'>
 			<h1 className='Headline_text App_light Headline_large'>Anthony Volk.</h1>
@@ -37,11 +77,13 @@ export default function Headline() {
           )}
 				>
 					<h1 className='Headline_text App_light Headline_small'>Full-Stack Developer.
-            <span className={`Headline_text App_light Headline_small Headline_hint Headline_hint--1 ${isMouseOver && 'Headline_text--hidden'}`}>.
-            </span>
-            <span className={`Headline_text App_light Headline_small Headline_hint Headline_hint--2 ${isMouseOver && 'Headline_text--hidden'}`}>.
-            </span>
-            <span className={`Headline_text App_light Headline_small Headline_hint Headline_hint--3 ${isMouseOver && 'Headline_text--hidden'}`}>.
+            <span className='Headline_hintWrapper'>
+              <span className={`Headline_text App_light Headline_small Headline_hint Headline_hint--1 ${isMouseOver && 'Headline_text--hidden'}`}>.
+              </span>
+              <span className={`Headline_text App_light Headline_small Headline_hint Headline_hint--2 ${isMouseOver && 'Headline_text--hidden'}`}>.
+              </span>
+              <span className={`Headline_text App_light Headline_small Headline_hint Headline_hint--3 ${isMouseOver && 'Headline_text--hidden'}`}>.
+              </span>
             </span>
           </h1>
 					<h1 className={`Headline_text Headline_small App_red ${!isMouseOver && `Headline_text--hidden`}`}>Ethical Technologist.</h1>
@@ -51,8 +93,13 @@ export default function Headline() {
 				</div>
 				<div 
 					className='Headline_scrollWrapper' 
-					onMouseOver={handleMouseOver} 
-					onMouseOut={handleMouseOut}
+          {...(breakpoint === 'mobile'
+            ? { onClick: handleClick }
+            : { 
+              onMouseOver: handleMouseOver, 
+              onMouseOut: handleMouseOut
+            }
+          )}
 				>
 					<h1 className={`Headline_text Headline_small App_light ${!isMouseOver && `Headline_text--hidden`}`}>Full-Stack Developer.</h1>
 					<h1 className={`Headline_text Headline_small App_red ${!isMouseOver && `Headline_text--hidden`}`}>Ethical Technologist.</h1>
