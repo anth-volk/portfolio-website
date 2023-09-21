b9374036547bbe89d8e47c4dfe805101264eedbf
Anthony Volk <anth.volk@gmail.com>
2023-09-20 19:24:39 -0600

    feat: Experience skeleton with data

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,12 +1,56 @@
 
 // Internal imports
 import '../styles/Experience.css';
+import { experienceData } from '../data/experienceData.js';
 
 export default function Experience() {
+
+  const resume = constructResume(experienceData);
 
 	return (
 
 		<section className='Experience' id='experience'>
+      <h1>Experience</h1>
+      {resume}
 		</section>
 	);
+}
+
+function constructResume(experienceData) {
+
+  const resume = experienceData.map((position) => {
+
+    const company = (<h2>{position.company}</h2>);
+    const title = (<p>{position.title}</p>);
+    const dateRange = (<p>{position.dateRange}</p>);
+
+    const tags = position.tags.map((tag) => {
+      return (<p>{tag}</p>)
+    });
+
+    const description = position.bullets.map((bullet) => {
+      return (<ul>{bullet}</ul>)
+    });
+
+    return (
+      <>
+        <div className="Experience_resItem_left">
+          {company}
+          {title}
+          {dateRange}
+          <div className="Experience_resItem_tagWrapper">
+            {tags}
+          </div>
+        </div>
+        <div className="Experience_resItem_right">
+          {description}
+        </div>
+      </>
+    );
+
+  });
+
+  return resume;
+
+
 }
---
+++ src/data/experienceData.js
@@ -0,0 +1,69 @@
+export const experienceData = [
+  {
+    company: "PolicyEngine",
+    title: "Full-Stack Developer",
+    dateRange: "April 2023 - present",
+    tags: [
+      "react",
+      "javascript",
+      "sql",
+      "python",
+      "flask",
+      "test-driven",
+      "project management"
+    ],
+    bullets: [
+      `Provide planning, development, implementation,
+      and maintenance of a full-stack open-source web
+      application implemented in ReactJS, SQL, Python
+      Flask, and OpenFisca`,
+      `Solved critical cross-stack data error, preventing
+      obsolescence of ~80% of user profiles`,
+      `Developing E2E testing infrastructure in React
+      and Python utilizing Jest, Pytest, and Cypress`,
+      `Developed light-touch project management framework
+      for ongoing front-end redesign`
+    ]
+  },
+  {
+    company: "Institute for State Effectiveness",
+    title: "Research Lead, Regional Connectivity & Infrastructure",
+    dateRange: "July 2018 - October 2022",
+    tags: [
+      "project management",
+      "strategy",
+      "research & analysis",
+      "client-facing",
+      "reporting"
+    ],
+    bullets: [
+      `Co-led, researched, and wrote three reports 
+      analyzing country service delivery constraints 
+      over 1-year+ time horizons that spawned 
+      policymaker-facing work in at least one country`,
+      `Produced multiple analytical written reports, 
+      scenarios papers, and policy recommendations to 
+      policymakers on the future of Asia-Pacific 
+      infrastructure development, including digital 
+      connectivity`
+    ]
+  },
+  {
+    company: "Japan Exchange & Teaching Programme",
+    title: "Assistant Language Teacher",
+    dates: "August 2019 - August 2020",
+    tags: [
+      "project management",
+      "planning",
+      "education",
+      "japanese"
+    ],
+    bullets: [
+      `Assisted English teachers of Japanese origin 
+      to deliver English language educational 
+      programming to classes of ~30 students`,
+      `Worked across cultural and linguistic barriers, 
+      with majority of work conducted in Japanese`
+    ]
+  }
+];
